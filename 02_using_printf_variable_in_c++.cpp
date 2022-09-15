b6974a3b33db2660b9c01afce9d6fa35c8ac2786
BHAVSARHINAL <110092239+BHAVSARHINAL@users.noreply.github.com>
2022-09-15 03:58:06 -0700

    Add files via upload

---
+++ 02_using_printf_variable_in_c++.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+
+int main()
+{
+	 int x=500,y=100;
+	 float c=34.56;
+	 char ch='9';
+	 
+	 
+   // cout<<x;
+//	cout<<y;
+	 
+//	cout<<x<<endl<<y;
+	
+//	cout<<"x=";
+//	cout<<x;
+//	cout<<" y=";
+//	cout<<y;
+
+ cout<<"x="<<x<<endl<<"y="<<y<<endl<<"c="<<c<<endl<<"ch="<<ch;
+
+
+return 0;
+
+
+
+}
---
+++ 03_using_scanf_in_c++.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int j;
+	float k;
+//	scanf("%f",k);
+//	printf("%f",k);
+	
+	
+//	cin>>j;
+//	cout<<j;
+	
+// cin>>j;
+// cin>>k;	
+	
+	cout<<"enter value of j nd k :";
+	cin>>j>>k;
+	
+	cout<<j<<endl<<k;
+	
+	
+	
+	
+	return 0;
+	
+ } 
