2acfe665febde492e064212e6a8a090629c54194
BHAVSARHINAL <110092239+BHAVSARHINAL@users.noreply.github.com>
2022-09-15 03:56:50 -0700

    Add files via upload

---
+++ 01_first_c++_program.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+//	cout<<"hii\n";
+//	cout<<"my name is...\n";
+//	cout<<"some name\n";
+	
+  //cout<<" hello world\n how are you\n fine";	
+//	cout<<"hy guys"<<endl<<"how are you"<<endl<<"fine";
+	
+	cout<<"good morning ..."<<"\n"<<"   to all";
+	
+	return 0;
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ 1_prectise.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	
+//	printf("hello....");
+	
+//	cout<<"hello\n";
+//	cout<<"hi how are you\n";
+//	cout<<"all are fine";
+	
+//	cout<<"hello world"<<"\n"<<"what are you doing??";
+	
+    cout<<"hii"<<endl<<"my dear friends"<<endl<<"what's upp";	
+	
+	return 0;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
